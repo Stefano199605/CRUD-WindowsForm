a39772840a902a1f495335d93830970dc9e2fefb
Stefano199605 <kevin.saldana1996@gmail.com>
2022-03-27 21:35:19 -0500

    Update DataAccesLayer
    Asignacion de parametros a la clase Contact

--- CRUD_SQLServer/CRUD_SQLServer/DataAccessLayer.cs
+++ CRUD_SQLServer/CRUD_SQLServer/DataAccessLayer.cs
@@ -9,7 +9,31 @@ namespace CRUD_SQLServer
 {
     public class DataAccessLayer
     {
-        private SqlConnection conn = new SqlConnection();
+        private SqlConnection conn = new SqlConnection("Server=DESKTOP-3LUEQ59;DataBase=WinFormsContact;UID=sa;password=p0deroso12");
+
+        public void InsertContact(Contacts contact)
+        {
+            try
+            {
+                conn.Open();
+                string query = @"Insert Into Contacts(FirstName,LastName,Phone,Address) VALUES(@FirstName,@LastName,@Phone,@Address)";
+                SqlParameter firstName = new SqlParameter();
+                firstName.ParameterName = "@FirstName";
+                firstName.Value = contact.FirstName;
+                firstName.DbType =System.Data.DbType.String;
+
+                SqlParameter LastName = new SqlParameter("@LastName",contact.LastName);
+                SqlParameter Phone = new SqlParameter("@Phone", contact.Phone);
+                SqlParameter Address = new SqlParameter("@Address", contact.Address);
+
+                SqlCommand command = new SqlCommand(query, conn);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
 
     }
 }
